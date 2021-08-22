c2e0dddbffee878e29c91eabfd9d9b8f0211a576
Scionax <ScionaxNG@gmail.com>
2021-08-22 01:12:25 -0500

    Adding New Scan Types

--- controller/ForumController.ts
+++ controller/ForumController.ts
@@ -14,8 +14,13 @@ export default class ForumController extends WebController {
 		return await conn.sendFail("Method Not Allowed", 405);
 	}
 	
-	// GET /forum/:forum?s=10
-	//		- s: Refers to Start Position (for pagination). So s=10 would start at index 10.
+	// GET /forum/:forum?h=10
+	//		- h: Refers to High ID limit, to identify posts the user has cached. So h=10 indicates the user's highest cached ID is 10.
+	//		- l: Refers to Low ID limit, to identify posts the user has cached. So l=5 indicates the user's lowest cached ID is 5.
+	//		- s: Refers to 'Scan' type: new (default), asc (ascending), or desc (descending).
+	//			- 'new' scan means we're searching for the newest results, and stopping if we hit the High ID.
+	//			- 'asc' scan means we're searching upward from the High ID.
+	//			- 'desc' scan means we're searching downward from the Low ID.
 	async getController(conn: Conn): Promise<Response> {
 		
 		// Make sure the forum exists
@@ -23,32 +28,61 @@ export default class ForumController extends WebController {
 			return await conn.sendFail("Forum Request: Forum does not exist.");
 		}
 		
-		let startId = 0;
-		const count = 10;
+		let highId = 0;
+		let lowId = 0;
+		let scan = 'new';
+		let count = 25;
+		let scanStartId = 0;
 		
 		// Get the user's query string parameters.
 		for(const [key, value] of conn.url.searchParams.entries()) {
-			if(key === "s") {
-				const s = Number(value);
-				if(s > 0) { startId = s; }
+			if(key === "h") {
+				const h = Number(value);
+				if(h > 0) { highId = h; }
+			}
+			else if(key === "l") {
+				const l = Number(value);
+				if(l > 0) { lowId = l; }
+			}
+			else if(key === "s") {
+				if(value === "desc") { scan = "desc"; }
+				else if(value === "asc") { scan = "asc"; }
 			}
 		}
 		
-		// If the user didn't specify a ?s= parameter (for starting pagination ID), get the newest set available.
-		if(startId === 0) {
-			startId = (await RedisDB.getForumPostId(conn.url2)) - count;
-			if(startId < 0) { startId = 0; }
+		// Get the Starting ID and the Count (the number of indexes to scan above the Starting ID)
+		if(scan === "desc") {
+			scanStartId = lowId - count;
+			
+			if(scanStartId < 0) {
+				scanStartId = 0;
+				count = lowId;
+			}
+		}
+		else if(scan === "asc") {
+			scanStartId = highId;
+		} else {
+			const newestId = await RedisDB.getForumPostId(conn.url2);
+			scanStartId = newestId - count;
+			
+			if(highId > scanStartId) {
+				scanStartId = highId + 1;
+				count = newestId - scanStartId;
+			}
 		}
 		
 		const postResults: unknown[] = [];
 		
-		// Retrieve the Index
-		const index = await RedisDB.getById_Post_Forum(conn.url2, startId, count);
-		
-		// Pipelines return absolutely f*#@ing idiotic data, so I guess we loop here. Maybe someday we can optimize this.
-		for(let i = 0; i < index.length; i++) {
-			const obj = await RedisDB.getHashTable(`post:${index[i]}`);
-			postResults.push(obj);
+		if(count > 0) {
+			
+			// Retrieve the Forum Index
+			const index = await RedisDB.getById_Post_Forum(conn.url2, scanStartId, count);
+			
+			// Pipelines return absolutely f*#@ing idiotic data, so I guess we loop here. Maybe someday we can optimize this.
+			for(let i = 0; i < index.length; i++) {
+				const obj = await RedisDB.getHashTable(`post:${index[i]}`);
+				postResults.push(obj);
+			}
 		}
 		
 		return await conn.sendJson(postResults);
